378ab0fffd3f57ae06be21e22f5e21479fa23a76
iddoeldor <iddo.eldor@startapp.com>
2017-08-22 17:39:23 +0300

    init 2

--- src/main/java/com/ie/CrosswordApplication.java
+++ src/main/java/com/ie/CrosswordApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ImportResource;
  * https://spring.io/guides/gs/actuator-service/
  * https://devcenter.heroku.com/articles/deploying-spring-boot-apps-to-heroku#preparing-a-spring-boot-app-for-heroku
  * /home/iddo/.ssh/id_rsa.pub
+ * https://cryptic-hollows-99859.herokuapp.com/ | https://git.heroku.com/cryptic-hollows-99859.git
  */
 @SpringBootApplication
 @ImportResource("classpath:config.xml")
