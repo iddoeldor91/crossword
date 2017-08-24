a407797f793eeef57c9dc6c544d6b67eda1a72ba
iddoeldor <iddo.eldor@startapp.com>
2017-08-24 12:48:25 +0300

    init 5

--- src/main/java/com/ie/services/CsvWordBankService.java
+++ src/main/java/com/ie/services/CsvWordBankService.java
@@ -95,7 +95,7 @@ public class CsvWordBankService implements WordBankService {
             }
         });
         stream.close();
-        wordBankList = wordBankList.subList(0, 50);
+        wordBankList = wordBankList.subList(0, 30);
         List<Word> wordList = wordBankList.stream().map(w -> new Word(w, null)).collect(Collectors.toList());
         try {
             initClues(wordList);
