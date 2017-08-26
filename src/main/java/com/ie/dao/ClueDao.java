8eb37506fe86fea47fc746c51646940146740802
iddoeldor <iddo.eldor@startapp.com>
2017-08-26 20:36:46 +0300

    adding 3 attempts to get clue

--- src/main/java/com/ie/dao/ClueDao.java
+++ src/main/java/com/ie/dao/ClueDao.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Base64;
 
@@ -19,13 +20,18 @@ public class ClueDao {
 
     private static final Logger LOG = LoggerFactory.getLogger(ClueDao.class);
 
-    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
-    private static final String IMAGE_PROVIDER_URL = "https://www.google.co.il/search?site=imghp&tbm=isch&source=hp&gws_rd=cr&tbs=isz:m&q=";
-    private static final String REFERRER = "https://www.google.co.il/";
+    private static final String CACHE_KEY = "clues";
+
+    private static final String IMAGE_PROVIDER_API = "https://www.google.co.il/search?site=imghp&tbm=isch&source=hp&gws_rd=cr&tbs=isz:m&q=";
+    // faking user agent
+    private static final String UA = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+    // fake referee
+    private static final String REF = "https://www.google.co.il/";
     private static final String IMAGE_QUERY_SELECTOR = "div.rg_meta";
     private static final String URL_MAP_KEY = "ou";
-    private static final int TIMEOUT_MS = 3_000;
-    private static final String CACHE_KEY = "clues";
+    // request timeout in milliseconds
+    private static final int TO = 3_000;
+    private static final int ATTEMPT_COUNTER = 3;
 
     /**
      *
@@ -37,20 +43,28 @@ public class ClueDao {
     @Cacheable(CACHE_KEY)
     public String getClue(String word) {
         String result = null;
+        int counter = 0;
+        LOG.debug("trying to fetch clue for word [{}]", word);
+        Elements elements = null;
         try {
-            LOG.debug("trying to fetch clue for word [{}]", word);
-            Document doc = Jsoup.connect(IMAGE_PROVIDER_URL + word)
-                    .userAgent(USER_AGENT).referrer(REFERRER).timeout(TIMEOUT_MS)
-                .get();
-            Elements elements = doc.select(IMAGE_QUERY_SELECTOR);
-            String firstElement = elements.first().childNodes().get(0).toString();
-            String imgUrl = JsonParserFactory.getJsonParser().parseMap(firstElement).get(URL_MAP_KEY).toString();
-            URL url = new URL(imgUrl);
-            byte[] imageBytes = IOUtils.toByteArray(url);
-            result = Base64.getEncoder().encodeToString(imageBytes);
-            LOG.info("fetched clue [{}] url [{}] length [{}]", word, imgUrl, result.length());
-        } catch (Exception e) {
-            LOG.error("Error fetching clue [{}] [{}]", word, e.getMessage());
+            Document doc = Jsoup.connect(IMAGE_PROVIDER_API + word).userAgent(UA).referrer(REF).timeout(TO).get();
+            elements = doc.select(IMAGE_QUERY_SELECTOR);
+        } catch (IOException e) {
+            LOG.error("failed fetching source for [{}]", word);
+        }
+        // source code exit, && haven't fetched clue url yet && not reached attempts limit
+        while (elements != null && result == null && counter < ATTEMPT_COUNTER) {
+            try {
+                String firstElement = elements.get(counter).childNodes().get(0).toString();
+                String imgUrl = JsonParserFactory.getJsonParser().parseMap(firstElement).get(URL_MAP_KEY).toString();
+                URL url = new URL(imgUrl);
+                byte[] imageBytes = IOUtils.toByteArray(url);
+                result = Base64.getEncoder().encodeToString(imageBytes);
+                LOG.info("fetched clue [{}] url [{}] length [{}]", word, imgUrl, result.length());
+            } catch (Exception e) {
+                LOG.error("Error fetching clue [{}] [{}]", word, e.getMessage());
+                counter++;
+            }
         }
         return result;
     }
